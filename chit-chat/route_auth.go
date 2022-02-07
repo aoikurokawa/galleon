f4a0d5b30d4c2eca9c812ebae6bcb06aa2475b53
Aoi <ht.so.ik0102@gmail.com>
2022-02-07 20:47:34 +0700

    change

---
+++ chit-chat/route_auth.go
@@ -0,0 +1,27 @@
+package main
+
+import "net/http"
+
+// POST /authenticate
+func authenticate(writer http.ResponseWriter, request *http.Request) {
+	err := request.ParseForm()
+	user, err := data.UserByEmail(request.PostFormValue("email"))
+	if err != nil {
+		danger(err, "Can not find user")
+	}
+	if user.Password == data.Encrypt(request.PostFormValue("password")) {
+		session, err := user.CreateSession()
+		if err != nil {
+			danger(err, "Can not create session")
+		}
+		cookie := http.Cookie{
+			Name:  "_cookie",
+			Value: session, Uuid,
+			HttpOnly: true,
+		}
+		http.SetCookie(writer, &cookie)
+		http.Redirect(writer, request, "/", 302)
+	} else {
+		http.Redirect(writer, request, "/login", 302)
+	}
+}
