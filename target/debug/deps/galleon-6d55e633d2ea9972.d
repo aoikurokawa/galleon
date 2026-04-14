5502805994877014ab9069827cfccecabc368bb0
aoikurokawa <ht.so.ik0102@gmail.com>
2026-04-15 07:02:49 +0900

    fix: delete target

--- target/debug/deps/galleon-6d55e633d2ea9972.d
+++
@@ -1,5 +0,0 @@
-/Users/aoikurokawa/dev/galleon/target/debug/deps/galleon-6d55e633d2ea9972.d: src/main.rs
-
-/Users/aoikurokawa/dev/galleon/target/debug/deps/libgalleon-6d55e633d2ea9972.rmeta: src/main.rs
-
-src/main.rs:
--- target/debug/deps/galleon-c805a0aae963f70a.d
+++
@@ -1,5 +0,0 @@
-/Users/aoikurokawa/dev/galleon/target/debug/deps/galleon-c805a0aae963f70a.d: src/main.rs
-
-/Users/aoikurokawa/dev/galleon/target/debug/deps/libgalleon-c805a0aae963f70a.rmeta: src/main.rs
-
-src/main.rs:
