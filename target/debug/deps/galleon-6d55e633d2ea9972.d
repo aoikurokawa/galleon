3d958c98e7776e1de4add1cb0548e8a61a6c4b98
aoikurokawa <ht.so.ik0102@gmail.com>
2026-04-15 06:58:55 +0900

    feat: initialize

---
+++ target/debug/deps/galleon-6d55e633d2ea9972.d
@@ -0,0 +1,5 @@
+/Users/aoikurokawa/dev/galleon/target/debug/deps/galleon-6d55e633d2ea9972.d: src/main.rs
+
+/Users/aoikurokawa/dev/galleon/target/debug/deps/libgalleon-6d55e633d2ea9972.rmeta: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/galleon-c805a0aae963f70a.d
@@ -0,0 +1,5 @@
+/Users/aoikurokawa/dev/galleon/target/debug/deps/galleon-c805a0aae963f70a.d: src/main.rs
+
+/Users/aoikurokawa/dev/galleon/target/debug/deps/libgalleon-c805a0aae963f70a.rmeta: src/main.rs
+
+src/main.rs:
